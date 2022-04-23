31122ffa9ab37189a713e2d4bf698c3fd6dfdb68
khaled hesham <kha.hesham@gmail.com>
2022-04-23 22:41:41 +0200

    Update Indexer.java

--- SearchEngine/Indexer/src/indexer/Indexer.java
+++ SearchEngine/Indexer/src/indexer/Indexer.java
@@ -7,20 +7,29 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.math3.util.Pair;
+
 public class Indexer {	
 
-	//HashMap<String ,HashMap<Integer ,List<Pair<String,Integer>>>> invertedIndex;
+	//private HashMap<String ,HashMap<Integer ,List<Pair<String,Integer>>>> invertedIndex;
 	public static void main(String[] args) throws IOException {
-		String URL = "https://www.imdb.com/title/tt0317219/";
+
+
+		//loop over all urls
+		
+		String URL = "https://www.imdb.com/title/tt0317219/"; //will be changed with url of each page
 		// "http://example.com/"
 		Parser_ parser_ = new Parser_(URL);
 		parser_.htmlToTextParse();
 		String[] myText = parser_.getCopyOfText_body();
-
+		
+		
 		List<String> without_unwanted_Symbols=parser_.Removing_Unwanted_Symbols(myText);	
 		List<String> without_stopWords=parser_.RemoveStopWords(without_unwanted_Symbols, "D:\\GITHUB\\Search-Engine\\SearchEngine\\Indexer\\src\\indexer\\stopwords.txt");
 
 		// HashMap<String ,List<int>> map=new HashMap
+
+		
 		
 		for(String word:without_stopWords)   //removing unwanted symbols + stopwords is done :D
 		{
