c3ce37451a0b019b73e726321151fc44290c3b72
khaled hesham <kha.hesham@gmail.com>
2022-04-23 20:12:50 +0200

    Update Indexer.java

--- SearchEngine/Indexer/src/indexer/Indexer.java
+++ SearchEngine/Indexer/src/indexer/Indexer.java
@@ -1,11 +1,15 @@
 package indexer;
 import indexer.Parser_;
+//import javafx.util.Pair;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class Indexer {	
+
+	//HashMap<String ,HashMap<Integer ,List<Pair<String,Integer>>>> invertedIndex;
 	public static void main(String[] args) throws IOException {
 		String URL = "https://www.imdb.com/title/tt0317219/";
 		// "http://example.com/"
@@ -15,6 +19,8 @@ public class Indexer {
 
 		List<String> without_unwanted_Symbols=parser_.Removing_Unwanted_Symbols(myText);	
 		List<String> without_stopWords=parser_.RemoveStopWords(without_unwanted_Symbols, "D:\\GITHUB\\Search-Engine\\SearchEngine\\Indexer\\src\\indexer\\stopwords.txt");
+
+		// HashMap<String ,List<int>> map=new HashMap
 		
 		for(String word:without_stopWords)   //removing unwanted symbols + stopwords is done :D
 		{
